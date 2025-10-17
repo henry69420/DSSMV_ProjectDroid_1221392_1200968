0f346ef7552e0f3979f13f3627107b35f1cb4ebf
henrique222 <1200968@isep.ipp.pt>
2025-10-17 08:38:21 +0100

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,4 +37,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 }
